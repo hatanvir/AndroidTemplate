e6aa7cf294af0dc80793fb0c2b5f9214f63f63f1
hatanvirintele <mhasan@intelehealth.org>
2024-01-27 21:52:57 +0600

    Bottom navigation setup, design update

--- app/src/main/java/com/tvr/androidtemplate/features/home/HomeActivity.kt
+++ app/src/main/java/com/tvr/androidtemplate/features/home/HomeActivity.kt
@@ -1,15 +1,14 @@
 package com.tvr.androidtemplate.features.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.navigation.NavController
 import com.tvr.androidtemplate.R
 import com.tvr.androidtemplate.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +18,7 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +27,14 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        setupBottomNavigation()
+        //setSupportActionBar(binding.toolbar)
 
         //viewModel.getPost()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
 
         /*binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -42,6 +43,26 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+
+    /**
+     * populating bottom navigation here
+     */
+    private fun setupBottomNavigation() {
+       binding.bottomNavigationView.setOnItemSelectedListener {
+           when(it.itemId){
+               R.id.action_post-> setFragment(R.id.PostFragment)
+               R.id.action_albums-> setFragment(R.id.PhotoFragment)
+               R.id.action_photos-> setFragment(R.id.PhotoFragment)
+              else -> setFragment(R.id.PostFragment)
+           }
+       }
+    }
+
+    private fun setFragment(id: Int): Boolean{
+        navController.navigate(id)
+        return true
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -58,6 +79,7 @@ class HomeActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
---
+++ app/src/main/java/com/tvr/androidtemplate/features/home/bindings/HomeBinding.kt
@@ -0,0 +1,8 @@
+package com.tvr.androidtemplate.features.home.bindings
+
+import androidx.databinding.BindingAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeBinding{
+
+}
